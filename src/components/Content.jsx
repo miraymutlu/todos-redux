f2d9db92b6caf04a98aee5c5a0b53db7c42ecaa4
miray <miray.mutlu@ozu.edu.tr>
2022-08-12 12:14:25 +0300

    redux store created

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -5,13 +5,12 @@ import ContentFooter from "./contentComponents/ContentFooter";
 function Content() {
   return (
     <>
-        <section className="main">
-          <input className="toggle-all" type="checkbox" />
-          <label htmlFor="toggle-all">Mark all as complete</label>
-          <List />
-    
-        </section>
-              <ContentFooter />
+      <section className="main">
+        <input className="toggle-all" type="checkbox" />
+        <label htmlFor="toggle-all">Mark all as complete</label>
+        <List />
+      </section>
+      <ContentFooter />
     </>
   );
 }

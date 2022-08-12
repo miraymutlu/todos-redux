71a33d9e5f7057cae04383d06ad4f1f1a91c5083
miray <miray.mutlu@ozu.edu.tr>
2022-08-12 11:42:49 +0300

    form component created

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,11 @@
 import React from "react";
+import Form from "./headerComponents/Form";
 
 function Header() {
   return (
     <header className="header">
       <h1>todos</h1>
-      <form>
-        <input
-          className="new-todo"
-          placeholder="What needs to be done?"
-          autoFocus
-        />
-      </form>
+      <Form />
     </header>
   );
 }
---
+++ src/components/headerComponents/Form.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Form() {
+  return (
+    <form>
+        <input
+          className="new-todo"
+          placeholder="What needs to be done?"
+          autoFocus
+        />
+      </form>
+  )
+}
+
+export default Form

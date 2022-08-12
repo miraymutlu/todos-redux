b0bc02e37823c491cae960bc48b29d313ee40785
miray <miray.mutlu@ozu.edu.tr>
2022-08-12 14:35:25 +0300

    toggle added

--- src/components/contentComponents/List.jsx
+++ src/components/contentComponents/List.jsx
@@ -1,8 +1,11 @@
 import React from "react";
-import {useSelector} from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { toggle } from "../../redux/todos/todosSlice";
 
-function List(){
-  const items = useSelector((state)=> state.todos.items);
+function List() {
+  const items = useSelector((state) => state.todos.items);
+  const dispatch = useDispatch();
+
   return (
     <ul className="todo-list">
       {/* <li className="completed">
@@ -12,14 +15,19 @@ function List(){
           <button className="destroy" />
         </div>
       </li> */}
-      {items.map(item=>(
-              <li key={item.id} className={item.completed ?"completed" : ""}>
-              <div className="view">
-                <input className="toggle" type="checkbox" />
-                <label>{item.title}</label>
-                <button className="destroy" />
-              </div>
-            </li>
+      {items.map((item) => (
+        <li key={item.id} className={item.completed ? "completed" : ""}>
+          <div className="view">
+            <input
+              className="toggle"
+              type="checkbox"
+              checked={item.completed}
+              onChange={() => dispatch(toggle({ id: item.id }))}
+            />
+            <label>{item.title}</label>
+            <button className="destroy" />
+          </div>
+        </li>
       ))}
     </ul>
   );

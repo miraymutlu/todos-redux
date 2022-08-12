932c33bf60cc245794365a5519bf23a5c84763df
miray <miray.mutlu@ozu.edu.tr>
2022-08-12 12:02:46 +0300

    content footer created

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,16 +1,19 @@
-import React from 'react'
-import List from './contentComponents/List'
+import React from "react";
+import List from "./contentComponents/List";
+import ContentFooter from "./contentComponents/ContentFooter";
 
 function Content() {
   return (
-    <section className="main">
-        <input className="toggle-all" type="checkbox" />
-        <label htmlFor="toggle-all">
-          Mark all as complete
-        </label>
-        <List />
-      </section>
-  )
+    <>
+        <section className="main">
+          <input className="toggle-all" type="checkbox" />
+          <label htmlFor="toggle-all">Mark all as complete</label>
+          <List />
+    
+        </section>
+              <ContentFooter />
+    </>
+  );
 }
 
-export default Content
+export default Content;
